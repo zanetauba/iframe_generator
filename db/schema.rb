d7e0932ac9900e0f78af223e7a903ada6530109a
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2020-02-23 20:59:45 +0200

    single iframe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_194042) do
+ActiveRecord::Schema.define(version: 2020_02_23_185818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "singles", force: :cascade do |t|
+    t.text "element"
+    t.text "iframe"
+    t.string "comment"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_singles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_02_20_194042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "singles", "users"
 end
