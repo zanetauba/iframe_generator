7a72e87bc34534cbf79c53c07cef6fedc5e74cdb
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2020-02-23 21:37:49 +0200

    single controller

---
+++ app/controllers/single_controller.rb
@@ -0,0 +1,42 @@
+class SingleController < ApplicationController
+
+  def index         # GET /singles
+    @singles = Single.all
+  end
+
+  def show          # GET /singles/:id
+    @single = Single.find(params[:id])
+  end
+
+  def new           # GET /singles/new
+    @single = Single.new
+  end
+
+  def create        # POST /singles
+    @single = Single.new(single_params)
+    @single.user_id = current_user.id
+    @single.save
+  end
+
+  def edit          # GET /singles/:id/edit
+  end
+
+  def update        # PATCH /singles/:id
+    @single.update(params[:single])
+  end
+
+  def destroy       # DELETE /bookings/:id
+    @single.destroy
+  end
+
+private
+
+  def single_params
+    params.require(:single).permit(:element, :iframe, :comment)
+  end
+
+  def set_listing
+    @single = Single.find(params[:id])
+  end
+
+end
